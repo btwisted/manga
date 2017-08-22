53a5e16221524e5c2e847b67f0af0107cc62a1b9
btwisted <info@nayahsolutions.org>
2017-08-22 09:12:42 -0700

    Add files via upload

---
+++ mangaEn.js
@@ -0,0 +1,18 @@
+/*var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://community-manga-eden.p.mashape.com/list/1",
+  "method": "GET",
+  "headers": {
+    "x-mashape-key": "t52UtH7ViKmshZtf8ivUH8puxgJ0p1B9PXkjsnOna3Xz8S45P6",
+    "cache-control": "no-cache",
+    "postman-token": "67a843b7-e2f1-03ca-dde0-b2302f3cfaa0"
+  }
+};
+
+function mangaEn(){
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+}
+*\
